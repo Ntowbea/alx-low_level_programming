8a194d0c2fe63ebc44e93dff4cc9263d62123587
“Ntowbea” <ntowbae@gmail.com>
2023-09-22 06:36:05 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * Displays all the contents of a list_t linked list
+ * The parameter 'h' is a pointer pointing to the linked list's head
+ *
+ * Returns the count of nodes in the list.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t cnt = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n");
+		}
+
+		h = h->next;
+		cnt++;
+	}
+
+	return (cnt);
+}
