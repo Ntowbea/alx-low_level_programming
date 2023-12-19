e46e96de17416a914df492a4195f459f35f8997c
Ntowbea <ntowbae@gmail.com>
2023-12-19 10:18:48 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,17 +12,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		putchar(43);
 		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		putchar('0');
-		return (0);
+		putchar(45);
+		return (-1);
 	}
 	else
 	{
-		putchar('_');
-		return (-1);
+		putchar(48);
+		return (0);
 	}
 }
